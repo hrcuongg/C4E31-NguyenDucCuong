ae011576addee024a74a95c597df1aa2ded25773
Cuong Nguyen <hr.cuongg@gmail.com>
2019-04-18 22:31:21 +0700

    .

---
+++ Session1/index.js
@@ -0,0 +1,20 @@
+// let x = prompt("Nhap x");
+// let y = prompt("Nhap y");
+// console.log("Tong cua x va y la", tong);
+// Nhập vào prompt kiểu dữl liêu auto = string
+// const x = prompt("Nhap vao so thu 1");
+// const y = prompt("Nhap vao so thu 2");
+// console.log(Number(x) + Number(y));
+// Các bước khởi tạo chương trình js cơ bản
+// 1.Tạo file html
+// 2.Gõ html, chọn html:5 để hiển thị ra khung html
+// 3.Tạo file js 
+// 4.Nối 2 file với nhau bằng cách sử dụng thẻ <script src=""></script>
+// 5. Chạy file html
+// 6. Inspect (Kiểm tra) trên trình duyệt 
+// 7. Chuyển qua tab console để xem kết quả
+
+// LOOPS:vòng lặp
+for(let i = 0; i < 10; i++) {
+    console.log(i);    
+}
